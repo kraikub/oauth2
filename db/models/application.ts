ab162c8a4f790be8f17d7644c12b1bc62c840de7
beam <nutchanon.chant@gmail.com>
2022-07-03 02:51:16 +0700

    fix: mongoose model overrides fail.

--- db/models/application.ts
+++ db/models/application.ts
@@ -1,4 +1,4 @@
 import { model, models } from "mongoose";
 import { Application, applicationSchema } from "../schema/application";
 
-export const ApplicationModel = models.Application<Application> ||  model<Application>('Application', applicationSchema, 'applications');
+export const ApplicationModel = models["Application"] ||  model<Application>('Application', applicationSchema, 'applications');
