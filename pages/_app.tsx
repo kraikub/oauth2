a89168e95381c0046cb77f8bb476cc03b4e09c34
Nutchanon Chantrasup <nutchanon.chant@gmail.com>
2022-07-17 20:50:20 +0700

    debug log: print configured env.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
-
+console.log(process.env)
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
