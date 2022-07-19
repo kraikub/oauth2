1e87a7fb07edabfb10d313244e58c3527ea55559
FranKydeSU <franapasin.s@gmail.com>
2022-07-19 23:06:24 +0700

    init ClientId

---
+++ pages/projects/manager/[clientId].tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import ClientPage from "../../../src/views/projects/manager/client";
+
+const Client: NextPage = () => {
+    return(
+        <ClientPage />
+    );
+}
+
+export default Client;
---
+++ src/views/projects/manager/client/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { FC } from "react";
+
+const ClientPage: FC = () => {
+    return(
+        <>
+        hello
+        </>
+    );
+}
+
+export default ClientPage;
