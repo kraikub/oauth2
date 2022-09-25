af85f7e46aa8eb2de890374d65693d0876105055
Nutchanon Chantrasup <nutchanon.chant@gmail.com>
2022-09-25 18:39:08 +0700

    🐛 fix build failed from import.

--- src/views/projects/manager/dashboard/index.tsx
+++ src/views/projects/manager/dashboard/index.tsx
@@ -3,7 +3,6 @@ import { NextPage } from "next";
 import Navbar from "../../../../layouts/Navbar";
 import { UserProvider } from "../../../../contexts/User";
 import AppTable from "./components/AppTable";
-import ProjectOwner from "./components/ProjectOwner";
 
 export const ProjectManagerDashboard: NextPage = () => {
   return (
